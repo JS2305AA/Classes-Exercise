36aa1103b9b4138e31739629b9be8dbc973b9097
2305 <npjt2305@yahoo.com>
2022-10-28 16:50:32 -0400

    Exercise Complete

---
+++ Classes/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+
+        public Car ()
+        {
+
+        }
+
+        //Special member method, same name as it's class.
+        //Create a make property that is of type string.
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        
+        public string Make { get; set; } //get = read, and set = write. 
+        public string Model { get; set; } //These 3 lines make and define Car. 
+        public int Year { get; set; }
+    }
+   
+}
